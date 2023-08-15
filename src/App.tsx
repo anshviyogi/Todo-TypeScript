105acc56ab5bc0c6faf4856c533af1f655c2f93f
ansh.viyogi <ansh.viyogi@inadev.com>
2023-08-15 14:11:45 +0530

    TODO TYPESCRIPT

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+type Todo = {
+  id: string | number;
+  todoTitle: string;
+};
 
 function App() {
+  const [state, setState] = useState<string>("");
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  const submitHandler = (e: any): Array<Todo> | void => {
+    e.preventDefault();
+
+    if (!state) {
+      // returning void
+      return alert("Type a Todo");
+    }
+
+    const obj = {
+      id: uuidv4(),
+      todoTitle: state,
+    };
+
+    setTodos([...todos, obj]);
+
+    setState("");
+
+    return todos;
+  };
+
+  const deleteHandler = (id: string | number): Array<Todo> | void => {
+    const filtered = todos.filter((item) => item.id !== id);
+
+    setTodos(filtered);
+    return todos;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODO APP</h1>
+      <form>
+        <input
+          placeholder="Your Todo"
+          value={state}
+          onChange={(e) => {
+            setState(e.target.value);
+          }}
+        />
+        <button onClick={submitHandler}>Submit</button>
+      </form>
+
+      {todos?.map((item) => (
+        <div style={{ display: "flex" }}>
+          <h1>{item.todoTitle}</h1>
+          <button onClick={() => deleteHandler(item.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
